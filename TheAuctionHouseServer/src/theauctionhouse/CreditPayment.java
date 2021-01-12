dc57a34394cb14e3d1cab5090fa0624653194c1d
DemonicOverlord <moddy-95@hotmail.com>
2021-01-13 01:19:31 +0200

    u

---
+++ TheAuctionHouseServer/src/theauctionhouse/CreditPayment.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package theauctionhouse;
+
+/**
+ *
+ * @author ur mama
+ */
+public class CreditPayment implements payMethod{
+
+    @Override
+    public void pay(int amount, String sender, String reciever) {
+        ;
+    }
+    
+}
---
+++ TheAuctionHouseServer/src/theauctionhouse/payMethod.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package theauctionhouse;
+
+/**
+ *
+ * @author mama
+ */
+public interface payMethod {
+    public void pay(int amount, String sender, String reciever);
+}
