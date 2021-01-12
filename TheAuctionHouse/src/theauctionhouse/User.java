27842a206a489b7258d8da7d52e3da85550d4c2d
PrinceWanted <56977987+PrinceWanted@users.noreply.github.com>
2021-01-13 00:14:59 +0200

    user and rename

---
+++ TheAuctionHouse/src/theauctionhouse/User.java
@@ -0,0 +1,10 @@
+package theauctionhouse;
+
+public class User {
+    private int uID;
+    private String Uname;
+    private String Upass;
+    private String Umail;
+    private int Uage;
+    private String Gender;
+}
